d2277a307f0525f113583075c9dd0e7bbd41fde2
Suresh Subedi <122662372+SureshSubedii@users.noreply.github.com>
2024-12-29 21:27:24 +0545

    Check dulicate occurences in an array

---
+++ Duplicate.java
@@ -0,0 +1,15 @@
+class Duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Boolean> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]))
+                return true;
+
+            map.put(nums[i], true);
+
+        }
+        return false;
+
+    }
+}
