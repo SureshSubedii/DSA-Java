fc14b3d273cf09a52dca14cae82ff7716aa63b0e
sureshsubedii <sureshsubedi485@gmail.com>
2025-02-06 16:52:59 +0545

    Implement topological sort by bfs

--- DirectedGraph.java
+++ DirectedGraph.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DirectedGraph {
         private Map<Integer, List<Integer>> adjList;
@@ -17,24 +14,55 @@ public class DirectedGraph {
         public void addEdge(int src, int dest){
             adjList.putIfAbsent(src, new ArrayList<>());
             adjList.get(src).add(dest);
+
             outDegree.put(src, outDegree.getOrDefault(src, 0) + 1);
             inDegree.put(dest, inDegree.getOrDefault(dest, 0) + 1);
+            inDegree.putIfAbsent(src, 0);
+
 
         }
         public void removeEdge(int src, int dest){
             if(adjList.containsKey(src)){
                 adjList.get(src).remove(Integer.valueOf(dest));
                 outDegree.put(src, outDegree.get(src) - 1);
-
                 inDegree.put(dest, inDegree.get(dest) - 1);
             }
         }
 
         public void printGraph(){
             for(var data: adjList.entrySet()){
-                System.out.println(data.getKey() + " -> " + data.getValue() + " inDegree = "  + this.inDegree.get(data.getKey()) +  " outDegree = "  + this.outDegree.get(data.getKey()));
+                System.out.println(data.getKey() + " -> " + data.getValue() + " inDegree = "  + Optional.ofNullable(this.inDegree.get(data.getKey()) ).orElse(0) +  " outDegree = "  + this.outDegree.get(data.getKey()));
+            }
+
+        }
+    public void topoSortBfs(){
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> topoOrder = new ArrayList<>();
+        for(int node: inDegree.keySet()){
+            if(inDegree.get(node) == 0){
+                queue.offer(node);
             }
         }
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            topoOrder.add(node);
+            if (adjList.containsKey(node)) {
+
+                for (int neighbour : adjList.get(node)) {
+                    inDegree.put(neighbour, inDegree.get(neighbour) - 1);
+                    if (inDegree.get(neighbour) == 0) {
+                        queue.offer(neighbour);
+                    }
+
+                }
+            }
+        }
+        if (topoOrder.size() != inDegree.size()) {
+            System.out.println("Graph has a cycle! Topological sorting is not possible.");
+            return;
+        }
+        System.out.println("Topological Sort (BFS): " + topoOrder);
+    }
 
     public static void main(String[] args) {
         DirectedGraph graph = new DirectedGraph();
@@ -43,16 +71,8 @@ public class DirectedGraph {
         graph.addEdge(0, 2);
         graph.addEdge(2, 5);
         graph.addEdge(2, 3);
-        graph.addEdge(2, 0);
-
-
-        graph.removeEdge(0, 1);
-
-        graph.printGraph();
-
-
-
-
+//        graph.addEdge(1, 0);
+        graph.topoSortBfs();
 
     }
 }
